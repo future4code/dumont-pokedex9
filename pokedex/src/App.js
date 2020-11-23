4e322fcf4b1a9af33a7b6bd5e94e15abc972855b
Lara-Mamedio <lmamedios@gmail.com>
2020-11-23 17:12:06 -0300

    visual esta incompleto ainda

---
+++ pokedex/src/App.js
@@ -0,0 +1,12 @@
+import Home from'./components/Home';
+
+class App extends React.Component {
+  render () {
+    return (
+      <Home></Home>
+
+    )
+  }
+}
+
+export default App;
---
+++ pokedex/src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {PokeCard} from './PokeCard';
+import image1 from '';
+
+export default class Home extends React.Component{
+    render () {
+        return(
+            <div>
+                <header>
+                    <button>Ver minha POKEDEX</button>
+                    <h1>Lista de Pokemons</h1>
+                </header>
+
+                <main className="CardContainer">
+                    <PokeCard
+                        image={image1}
+                    />
+                    
+                </main>
+            </div>
+        )
+    }
+}
